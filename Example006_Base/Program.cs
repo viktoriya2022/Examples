2b96dd76bb8ce04c8ade3260d5e539dec3392745
«Viktoriya <v.oberemko@mail.ru>
2022-07-13 13:37:29 +0300

    Add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 2;
+int c = 7;
+int d = 12;
+int e = 3;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
