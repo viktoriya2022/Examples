9b0804be6056966d87fbd208901da16c7c6aa351
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 20:35:47 +0300

    Add Example010_ArrayMethod

---
+++ Example010_ArrayMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 2, 34, 18, 61, 17, 18, 19 };
+
+int n=array.Length;
+int find = 18;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        // break;
+    }
+    index++;
+}
